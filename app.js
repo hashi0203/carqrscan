8a9659d3da3f0918cdebb5f9f4283b58ad0bc870
Glitch (hello-webpage) <none>
2020-05-20 08:26:10 +0000

    👋🎽 Checkpoint ./app.js:15135430/3104 ./index.html:15135430/2706

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var app = new Vue({
       this.scanner.start(camera);
     },
     decode: function(scans) {
-      var shakensho = {sd: null, kr: null, ym: null, st: null, ks1: null, ks2: null, zmm: null, zmu: null, zum: null, zuu: null, sk: null, ks: null, kh: null, os: null, np: null, no: null, pm: null, hk: null, ns: null, zt: null, hm: null, sb: null, gk: null, cs: null};
+      var shakensho = {sd: ["車台番号打刻位置"], kr: ["型式指定番号,類別区分番号"], ym: ["有効期間の満了する日"], st: ["初度登録年月"], ks1: ["型式"], ks2: ["型式(続き)"], zmm: ["軸重(前前)"], zmu: ["軸重(前後)"], zum: ["軸重(後前)"], zuu: ["軸重(後後)"], sk: ["騒音規制"], ks: ["近接排気騒音規制値"], kh: ["駆動方式"], os: ["オパシメータ測定車"], np: ["NOx・PM測定モード"], no: ["NOx値"], pm: ["PM値"], hk: ["保安基準適用年月日"], ns: ["燃料の種別コード"], zt: ["自動車登録番号"], hm: ["標板の枚数及び大きさ"], sb: ["車台番号"], gk: ["原動機型式"], cs: ["帳票種別"]};
       if (scans.length === 0) {
         return null;
       }
@@ -41,34 +41,34 @@ var app = new Vue({
         scan = scan.content.replace(/\s+/g, "").split('/');
         var l = scan.length;
         if (l == 6) {
-          shakensho['sd'] = scan[1];
-          shakensho['kr'] = scan[2];
-          shakensho['ym'] = scan[3];
-          shakensho['st'] = scan[4];
-          shakensho['ks1'] = scan[5];
+          shakensho['sd'].push(scan[1]);
+          shakensho['kr'].push(scan[2]);
+          shakensho['ym'].push(scan[3]);
+          shakensho['st'].push(scan[4]);
+          shakensho['ks1'].push(scan[5]);
         } else if (l == 7 && scan[1] != "" && scan[2] != "") {
-          shakensho['ks2'] = scan[0];
-          shakensho['zmm'] = scan[1];
-          shakensho['zmu'] = scan[2];
-          shakensho['zum'] = scan[3];
-          shakensho['zuu'] = scan[4];
-          shakensho['sk'] = scan[5];
+          shakensho['ks2'].push(scan[0]);
+          shakensho['zmm'].push(scan[1]);
+          shakensho['zmu'].push(scan[2]);
+          shakensho['zum'].push(scan[3]);
+          shakensho['zuu'].push(scan[4]);
+          shakensho['sk'].push(scan[5]);
         } else if (l == 8) {
-          shakensho['ks'] = scan[0];
-          shakensho['kh'] = scan[1];
-          shakensho['os'] = scan[2];
-          shakensho['np'] = scan[3];
-          shakensho['no'] = scan[4];
-          shakensho['pm'] = scan[5];
-          shakensho['hk'] = scan[6];
-          shakensho['ns'] = scan[7];
+          shakensho['ks'].push(scan[0]);
+          shakensho['kh'].push(scan[1]);
+          shakensho['os'].push(scan[2]);
+          shakensho['np'].push(scan[3]);
+          shakensho['no'].push(scan[4]);
+          shakensho['pm'].push(scan[5]);
+          shakensho['hk'].push(scan[6]);
+          shakensho['ns'].push(scan[7]);
         } else if (l == 2) {
-          shakensho['zt'] = scan[1];
+          shakensho['zt'].push(scan[1]);
         } else if (l == 5 && scan[0].length == 0 && scan[1].length == 1) {
-          shakensho['hm'] = scan[1];
-          shakensho['sb'] = scan[2];
-          shakensho['gk'] = scan[3];
-          shakensho['cs'] = scan[4];
+          shakensho['hm'].push(scan[1]);
+          shakensho['sb'].push(scan[2]);
+          shakensho['gk'].push(scan[3]);
+          shakensho['cs'].push(scan[4]);
         }
       }
       console.log(shakensho);
