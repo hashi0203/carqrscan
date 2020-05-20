a82f4ecde7a2bc491101c09603e9ec70a473f2c5
Glitch (hello-webpage) <none>
2020-05-20 05:54:04 +0000

    ⛈👽 Checkpoint ./instascan.min.js:15135430/1561617 ./index.html:15135430/259 ./script.js:15135430/2516

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var videoTag = document.getElementById('preview');
+var info = document.getElementById('info');
+var scanner = new Instascan.Scanner({ video: videoTag });
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+//QRコードを認識して情報を取得する
+scanner.addListener('scan', function (value) {
+  info.textContent = value;
+});
+
+//PCのカメラ情報を取得する
+Instascan.Camera.getCameras()
+.then(function (cameras) {
+
+    //カメラデバイスを取得できているかどうか？
+    if (cameras.length > 0) {
+
+      //スキャンの開始
+      scanner.start(cameras[0]);
+    }
+    else {
+      alert('カメラが見つかりません！');
+    }
+})
+.catch(function(err) {
+  alert(err);
+});
