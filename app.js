42a1a9c5eb6541434ab53dca6d71af52e7c2f1e5
Glitch (hello-webpage) <none>
2020-05-24 02:47:48 +0000

    👄🚙 Checkpoint ./index.html:15135430/21 ./app.js:15135430/254

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = new Vue({
     scanner: null,
     activeCameraId: null,
     cameras: [],
-    scans: []
+    scans: [],
+    qrs: [0,0,0,0,0,0,0,0]
   },
   mounted: function () {
     var self = this;
@@ -32,10 +33,10 @@ var app = new Vue({
       this.activeCameraId = camera.id;
       this.scanner.start(camera);
     },
-    decode: function(scans) {
+    decode: function() {
       var shakensho = {sd: ["車台番号打刻位置"], kr: ["型式指定番号,類別区分番号"], ym: ["有効期間の満了する日"], st: ["初度登録年月"], ks1: ["型式"], ks2: ["型式(続き)"], zmm: ["軸重(前前)"], zmu: ["軸重(前後)"], zum: ["軸重(後前)"], zuu: ["軸重(後後)"], sk: ["騒音規制"], ks: ["近接排気騒音規制値"], kh: ["駆動方式"], os: ["オパシメータ測定車"], np: ["NOx・PM測定モード"], no: ["NOx値"], pm: ["PM値"], hk: ["保安基準適用年月日"], ns: ["燃料の種別コード"], zt: ["自動車登録番号"], hm: ["標板の枚数及び大きさ"], sb: ["車台番号"], gk: ["原動機型式"], cs: ["帳票種別"]};
       if (scans.length === 0) {
-        return null;
+        return shakensho;
       }
       for (var scan of scans) {
         scan = scan.content.replace(/\s+/g, "").split('/');
@@ -46,6 +47,7 @@ var app = new Vue({
           shakensho['ym'].push(scan[3]);
           shakensho['st'].push(scan[4]);
           shakensho['ks1'].push(scan[5]);
+          qrs[1] = 1;
         } else if (l == 7 && scan[1] != "" && scan[2] != "") {
           shakensho['ks2'].push(scan[0]);
           shakensho['zmm'].push(scan[1]);
@@ -53,6 +55,7 @@ var app = new Vue({
           shakensho['zum'].push(scan[3]);
           shakensho['zuu'].push(scan[4]);
           shakensho['sk'].push(scan[5]);
+          qrs[2] = 1;
         } else if (l == 8) {
           shakensho['ks'].push(scan[0]);
           shakensho['kh'].push(scan[1]);
@@ -62,13 +65,16 @@ var app = new Vue({
           shakensho['pm'].push(scan[5]);
           shakensho['hk'].push(scan[6]);
           shakensho['ns'].push(scan[7]);
+          qrs[3] = 1;
         } else if (l == 2) {
           shakensho['zt'].push(scan[1]);
+          qrs[4] = 1;
         } else if (l == 5 && scan[0].length == 0 && scan[1].length == 1) {
           shakensho['hm'].push(scan[1]);
           shakensho['sb'].push(scan[2]);
           shakensho['gk'].push(scan[3]);
           shakensho['cs'].push(scan[4]);
+          qrs[5] = 1;
         }
       }
       console.log(shakensho);
