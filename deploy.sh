6865cd8cb5ad53777131b113b7239b22bf53da38
hashi0203 <n.hsmt1123@gmail.com>
2020-06-20 20:10:53 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
-sudo rm -r instascan
-git clone https://github.com/hashi0203/instascan.git instascan
-rm -r node_modules
+INSTA="instascan"
+if [ -d $INSTA ]; then
+	sudo rm -r ${INSTA}
+fi
+git clone https://github.com/hashi0203/instascan.git ${INSTA}
+MODULES="node_moudles"
+if [ -d $MODULES ]; then
+	rm -r $MODULES
+fi
 npm install
 browserify app.js -o bundle.js
 
